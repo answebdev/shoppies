003b061ab56054904986422dea67adb876d4103e
ANS <36783010+answebdev@users.noreply.github.com>
2021-04-29 18:31:29 -0700

    update code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchItem, setSearchItem] = useState('');
   const [nominate, setNominate] = useState([]);
-  const APIKEY = process.env.REACT_APP_MOVIE_API_KEY;
+
+  //const APIKEY = process.env.REACT_APP_MOVIE_API_KEY;
 
   useEffect(() => {
     fetchMovies(searchItem);
@@ -19,8 +20,6 @@ const App = () => {
 
   useEffect(() => {
     const moviesNominated = JSON.parse(localStorage.getItem('shoppies-movies'));
-    // setNominate(moviesNominated);
-
     if (moviesNominated) {
       setNominate(moviesNominated);
     }
@@ -31,14 +30,14 @@ const App = () => {
   };
 
   const fetchMovies = async (searchItem) => {
-    //const url = `https://www.omdbapi.com/?s=${searchItem}&apikey=1ec7b4c0`;
-    const url = `https://www.omdbapi.com/?s=${searchItem}&apikey=${APIKEY}`;
+    const url = `https://www.omdbapi.com/?s=${searchItem}&apikey=1ec7b4c0`;
+    //const url = `https://www.omdbapi.com/?s=${searchItem}&apikey=${APIKEY}`;
 
     const response = await fetch(url);
     const data = await response.json();
 
     if (data.Search) {
-      // console.log(data);
+      console.log(data);
       setMovies(data.Search);
     }
   };
--- src/components/NominatedMovies.js
+++ src/components/NominatedMovies.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RemoveNominatedMovie from './RemoveNominatedMovie';
 import classes from '../styles/NominatedMovies.module.css';
+import imdb from '../../src/img/imdb.png';
 
 const NominatedMovies = (props) => {
   return (
@@ -11,7 +12,19 @@ const NominatedMovies = (props) => {
             <div className={classes.MovieCard}>
               <p>{movie.Title}</p>
               <p>{movie.Year}</p>
+              <p>{movie.Rated}</p>
+              <p>{movie.Genre}</p>
               <img style={{ height: '100px' }} src={movie.Poster} alt='' />
+              <a
+                href={`https://www.imdb.com/title/${movie.imdbID.replace(
+                  /['"]+/g,
+                  ''
+                )}`}
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                <img style={{ width: '20%' }} src={imdb} alt='' />
+              </a>
             </div>
             <div onClick={() => props.handleRemove(movie)}>
               <RemoveNominatedMovie />
