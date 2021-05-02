4d8a6f74423b68f7ac5805130527744f4338032a
ANS <36783010+answebdev@users.noreply.github.com>
2021-05-01 20:29:41 -0700

    add react-transition-group

--- src/components/NominatedMovies.js
+++ src/components/NominatedMovies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import RemoveNominatedMovie from './RemoveNominatedMovie';
 import classes from '../styles/NominatedMovies.module.css';
 import imdb from '../../src/img/imdb.webp';
@@ -7,97 +8,56 @@ const NominatedMovies = (props) => {
   return (
     <div>
       <div className={classes.TopDiv}>
-        {/* <p className={classes.NominationsList}>
-          <strong>Your Nominations</strong>
-        </p> */}
-
         {props.movies.length > 0 ? (
           <p className={classes.NominationsList}>
             <strong>Your Nominations</strong>
           </p>
         ) : null}
-        {/* <div className={classes.NominationsListSub}>
-          <p>
-            Maximum 5 nominations allowed.
-            <br />
-            Movies Nominated: {props.movies.length}/5
-          </p>
-        </div> */}
-
         {props.movies.length === 5 ? props.restart() : null}
-
-        {/* {props.movies.length === 5 ? (
-          <button onClick={() => props.handleReset()}>RESET</button>
-        ) : null} */}
-
-        {/* {props.movies.length === 5
-        ? alert(
-            'You have reached the 5 film limit. Remove any film to select a new one.'
-          )
-        : null} */}
       </div>
 
-      {props.movies.map((movie, index) => (
-        <div key={index}>
-          <div className={classes.NomMovieDiv}>
-            <div className={classes.Child}>
-              <div>
-                <img className={classes.PosterImg} src={movie.Poster} alt='' />
-              </div>
-              <div className={classes.DetailsDiv}>
-                <p className={classes.MovieDetails}>
-                  {movie.Title} • {movie.Year}
-                </p>
-                <a
-                  href={`https://www.imdb.com/title/${movie.imdbID}`}
-                  target='_blank'
-                  rel='noreferrer noopener'
-                >
-                  <img className={classes.Imdb} src={imdb} alt='' />
-                </a>
-              </div>
+      <TransitionGroup>
+        {props.movies.map((movie, index) => (
+          <CSSTransition
+            key={movie.imdbID}
+            timeout={props.movies.length === 5 ? 0 : 500}
+            classNames={props.movies.length === 5 ? '' : 'item'}
+          >
+            <div key={index}>
+              <div className={classes.NomMovieDiv}>
+                <div className={classes.Child}>
+                  <div>
+                    <img
+                      className={classes.PosterImg}
+                      src={movie.Poster}
+                      alt=''
+                    />
+                  </div>
+                  <div className={classes.DetailsDiv}>
+                    <p className={classes.MovieDetails}>
+                      {movie.Title} • {movie.Year}
+                    </p>
+                    <a
+                      href={`https://www.imdb.com/title/${movie.imdbID}`}
+                      target='_blank'
+                      rel='noreferrer noopener'
+                    >
+                      <img className={classes.Imdb} src={imdb} alt='' />
+                    </a>
+                  </div>
 
-              <div
-                className={classes.NomBtn}
-                onClick={() => props.handleRemove(movie)}
-              >
-                <RemoveNominatedMovie isNominated={props.isNominated} />
+                  <div
+                    className={classes.NomBtn}
+                    onClick={() => props.handleRemove(movie)}
+                  >
+                    <RemoveNominatedMovie isNominated={props.isNominated} />
+                  </div>
+                </div>
               </div>
-
-              {/* <div>
-                  {!movie && !isLoading ? <div>Movie not found</div> : null}
-                </div> */}
             </div>
-          </div>
-        </div>
-      ))}
-      {/* {props.movies.map((movie, index) => (
-        <div className={classes.NominatedMovieCard} key={index}>
-          <div className={classes.MovieDiv}>
-            <div className={classes.MovieCard}>
-              <p>{movie.Title}</p>
-              <p>{movie.Year}</p>
-              <p>{movie.Rated}</p>
-              <p>{movie.Genre}</p>
-              <img style={{ height: '100px' }} src={movie.Poster} alt='' />
-              <a
-                href={`https://www.imdb.com/title/${movie.imdbID}`}
-                // href={`https://www.imdb.com/title/${movie.imdbID.replace(
-                //   /['"]+/g,
-                //   ''
-                // )}`}
-                target='_blank'
-                rel='noreferrer noopener'
-              >
-                <img style={{ width: '20%' }} src={imdb} alt='' />
-              </a>
-            </div>
-            <div onClick={() => props.handleRemove(movie)}>
-              <RemoveNominatedMovie />
-            </div>
-          </div>
-        </div>
-      ))} */}
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
